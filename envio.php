ffe4e4431aaa0d6ff3298493fff5f8203eadb24e
DESKTOP-C6UOPR4\Dani <dani15requena@gmail.com>
2018-04-29 14:46:05 +0200

    Prueba Solución envios Formulario

--- envio.php
+++ envio.php
@@ -4,10 +4,10 @@ $apelnombre = $_POST['apelnom'];
 $correo_electronico= $_POST['email']; 
 $texto = $_POST['mensaje'];
  
-$header = 'De: ' . $correo_electronico ."\r\n"; 
+/*$header = 'De: ' . $correo_electronico ."\r\n"; 
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n"; 
 $header .= "Mime-Version: 1.0 \r\n"; 
-$header .= "Content-Type: text/plain"; 
+$header .= "Content-Type: text/plain"; */
 
 $mensaje = "Este mensaje fue enviado por " . $apelnombre . " \r\n"; 
 $mensaje .= "Su e-mail es: " . $correo_electronico . " \r\n";
@@ -17,7 +17,7 @@ $mensaje .= "Enviado el " . date('d/m/Y', time());
 $para = 'supp.crmsys@gmail.com'; 
 $asunto = 'Correo Contacto'; 
 
-mail($para, $asunto, utf8_decode($mensaje), $header); 
+mail($para, $asunto,$mensaje); 
 
 header("Location:index.php");
 ?> 
