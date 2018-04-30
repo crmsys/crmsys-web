fdb2b478f4f12c146221de76cce104dd6d8f0252
Dani Requena <dani15requena@gmail.com>
2018-04-30 13:50:27 +0200

    Prueba de envio Mediante Clase PHPMailer

--- envio_old.php
+++ envio_old.php
@@ -1,10 +1,13 @@
 <?php 
-require("PHPMailer-FE_v4.11/_lib/class.phpmailer.php");
-$mail = new PHPMailer();
-
+error_reporting(0); 
 $apelnombre = $_POST['apelnom']; 
 $correo_electronico= $_POST['email']; 
 $texto = $_POST['mensaje'];
+ 
+$header = 'De: ' . $correo_electronico ."\r\n"; 
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n"; 
+$header .= "Mime-Version: 1.0 \r\n"; 
+$header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $apelnombre . " \r\n"; 
 $mensaje .= "Su e-mail es: " . $correo_electronico . " \r\n";
@@ -14,12 +17,7 @@ $mensaje .= "Enviado el " . date('d/m/Y', time());
 $para = 'supp.crmsys@gmail.com'; 
 $asunto = 'Correo Contacto'; 
 
-$mail­>SetFrom($para, 'CRMSYS');
-$mail­>AddAddress($correo_electronico, $apelnombre);
-$mail­>Subject = $asunto;
-$mail­>Body = $mensaje;
-
-$mail­>Send()
+mail($para, $asunto,utf8_decode($mensaje)); 
 
 header("Location:index.php");
 ?> 
