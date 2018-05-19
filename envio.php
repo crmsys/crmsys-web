afa37510217a1fbaaa20e7a8067a7923d0e03bd8
Dani Requena <dani15requena@gmail.com>
2018-05-19 20:25:44 +0200

    Cambios Casi Finales WEB

--- envio.php
+++ envio.php
@@ -4,50 +4,71 @@ require("PHPMailer-FE_v4.11/_lib/class.phpmailer.php");
 
 $mail = new PHPMailer();
 
-$mail ->isSMTP();
-$mail ->SMTPDebug  = 2;
-$mail ->Debugoutput = 'html';
-$mail ->SMTPAuth   = true;
-$mail ->SMTPSecure = "tsl";
-$mail ->Host       = "smtp.gmail.com";
-$mail ->Username   = "supp.crmsys@gmail.com";
-$mail ->Password   = "sudoaptgetmoo";
-$mail ->Port       = 587;
-$mail ->CharSet    = "UTF-8";
+$mail ->CharSet = "UTF-8";
+$mail ->IsHTML(true); 
 
 $apelnombre = $_POST['apelnom']; 
 $correo_electronico= $_POST['email']; 
 $texto = $_POST['mensaje'];
 
-$mensaje = "Este mensaje fue enviado por " . $apelnombre . " \r\n"; 
-$mensaje .= "Su e-mail es: " . $correo_electronico . " \r\n";
-$mensaje .= "Su mensaje es: " . $texto . " \r\n"; 
-$mensaje .= "Enviado el " . date('d/m/Y', time()); 
+$mensaje = "<b>Este mensaje fue enviado por: </b>" . $apelnombre . "<br>"; 
+$mensaje .= "<b>Su e-mail es: </b>" . $correo_electronico . "<br>";
+$mensaje .= "<b>Su mensaje es: </b>" . $texto . "<br>"; 
+$mensaje .= "<b>Enviado el: </b>" . date('d/m/Y', time()); 
 
 $para = 'supp.crmsys@gmail.com'; 
-$asunto = 'Correo Contacto'; 
 
 $mail ->From = $para;
-$mail ->AddAddress($correo_electronico);
-$mail ->AddAddress($para);
-$mail ->Subject = $asunto;
+$mail ->FromName = "CRMSYS";
+$mail ->AddAddress($para,"Nuevo Comentario");
+$mail ->Subject = 'Nueva Consulta (WEB)';
 $mail ->Body = $mensaje;
 
 if ($mail ->Send()) { 
-    /*echo " <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
+    echo " <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
 		   <link rel='stylesheet' type='text/css' href='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.css' />
 		   <script src='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.js'></script>
 		   <script type='text/javascript'>$.jGrowl('Correo Enviado Correctamente',{theme : 'bien'});</script> 
-		   <style type='text/css'>div.jGrowl div.bien {background-color: #77EF6A;color: #000;font-size:16px;}</style> "; */
-	echo "Mensaje Enviado";
+		   <style type='text/css'>div.jGrowl div.bien {background-color: #77EF6A;color: #000;font-size:16px;}</style> "; 
 }else{ 
-    /*echo " <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
+    echo " <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
 		   <link rel='stylesheet' type='text/css' href='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.css' />
 		   <script src='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.js'></script>
 		   <script type='text/javascript'>$.jGrowl('Correo Enviado Correctamente',{theme : 'bien'});</script> 
-		   <style type='text/css'>div.jGrowl div.bien {background-color: #FE2E2E;color: #000;font-size:16px;}</style> ";*/
-	echo "Error: ".$mail ->ErrorInfo;
+		   <style type='text/css'>div.jGrowl div.bien {background-color: #FE2E2E;color: #000;font-size:16px;}</style> ";
 }
 
-//header("Location:index.php");
+$mail = new PHPMailer();
+
+$mail ->CharSet = "UTF-8";
+$mail ->IsHTML(true);
+
+$mail ->AddEmbeddedImage('images/logo.png', 'imagen');
+$rcss = "assets/css/email.css";
+$fcss = fopen ($rcss, "r");//abrir archivo css
+$scss = fread ($fcss, filesize ($rcss));//leer contenido de css
+fclose ($fcss);
+$shtml = file_get_contents('email.html');
+$cuerpo  = str_replace('<style id="estilo"></style>',"<style>$scss</style>",$shtml); 
+
+$mail ->From = $para;
+$mail ->FromName = "CRMSYS";
+$mail ->AddAddress($correo_electronico,$apelnombre);
+$mail ->Subject = 'Bienvenid@ a CRMSYS';
+$mail ->Body = $cuerpo;
+
+if ($mail ->Send()) { 
+    echo " <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
+		   <link rel='stylesheet' type='text/css' href='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.css' />
+		   <script src='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.js'></script>
+		   <script type='text/javascript'>$.jGrowl('Correo Enviado Correctamente',{theme : 'bien'});</script> 
+		   <style type='text/css'>div.jGrowl div.bien {background-color: #77EF6A;color: #000;font-size:16px;}</style> ";
+}else{ 
+    echo " <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
+		   <link rel='stylesheet' type='text/css' href='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.css' />
+		   <script src='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.js'></script>
+		   <script type='text/javascript'>$.jGrowl('Correo Enviado Correctamente',{theme : 'bien'});</script> 
+		   <style type='text/css'>div.jGrowl div.bien {background-color: #FE2E2E;color: #000;font-size:16px;}</style> ";
+}
+header("Location:index.php");
 ?> 
--- envio_old.php
+++ envio_old.php
@@ -3,21 +3,65 @@ error_reporting(0);
 $apelnombre = $_POST['apelnom']; 
 $correo_electronico= $_POST['email']; 
 $texto = $_POST['mensaje'];
+$para = 'supp.crmsys@gmail.com'; 
+$asunto = 'Correo Contacto'; 
  
-$header = 'De: ' . $correo_electronico ."\r\n"; 
+$header = 'From: ' . $correo_electronico ."\r\n"; 
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n"; 
 $header .= "Mime-Version: 1.0 \r\n"; 
-$header .= "Content-Type: text/plain";
+$header .= "Content-Type: text/html;charset=UTF-8\r\n";
 
+$mensaje = "<b>Este mensaje fue enviado por: </b>" . $apelnombre . "<br>"; 
+$mensaje .= "<b>Su e-mail es: </b>" . $correo_electronico . "<br>";
+$mensaje .= "<b>Su mensaje es: </b>" . $texto . "<br>"; 
+$mensaje .= "<b>Enviado el: </b>" . date('d/m/Y', time()); 
+
+$mail = mail($para, $asunto,utf8_decode($mensaje),$header); 
+if ($mail) { 
+    echo " <html><head></head><body>
+		   <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
+		   <link rel='stylesheet' type='text/css' href='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.css' />
+		   <script src='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.js'></script>
+		   <script type='text/javascript'>$.jGrowl('Correo Enviado Correctamente',{theme : 'bien'});</script> 
+		   <style type='text/css'>div.jGrowl div.bien {background-color: #77EF6A;color: #000;font-size:16px;}</style></body></html> ";
+}else{ 
+    echo " <html><head></head><body> 
+		   <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
+		   <link rel='stylesheet' type='text/css' href='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.css' />
+		   <script src='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.js'></script>
+		   <script type='text/javascript'>$.jGrowl('Correo NO Enviado Correctamente',{theme : 'bien'});</script> 
+		   <style type='text/css'>div.jGrowl div.bien {background-color: #FE2E2E;color: #000;font-size:16px;}</style></body></html> ";
+}
+
+$header="";
+$header = 'From: ' . $para ."\r\n"; 
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n"; 
+$header .= "Mime-Version: 1.0 \r\n"; 
+$header .= "Content-Type: text/plain;charset=UTF-8\r\n";
+
+$mensaje="";
 $mensaje = "Este mensaje fue enviado por " . $apelnombre . " \r\n"; 
 $mensaje .= "Su e-mail es: " . $correo_electronico . " \r\n";
 $mensaje .= "Su mensaje es: " . $texto . " \r\n"; 
-$mensaje .= "Enviado el " . date('d/m/Y', time()); 
+$mensaje .= "Enviado el " . date('d/m/Y', time(). " \r\nGRACIAS");
+$mensaje .= "GRACIAS\r\n"; 
 
-$para = 'supp.crmsys@gmail.com'; 
-$asunto = 'Correo Contacto'; 
-
-mail($para, $asunto,utf8_decode($mensaje)); 
-
+$mail=null;
+$mail = mail($correo_electronico, $asunto,utf8_decode($mensaje),$header); 
+if ($mail) { 
+    echo " <html><head></head><body>
+		   <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
+		   <link rel='stylesheet' type='text/css' href='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.css' />
+		   <script src='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.js'></script>
+		   <script type='text/javascript'>$.jGrowl('Correo Enviado Correctamente',{theme : 'bien'});</script> 
+		   <style type='text/css'>div.jGrowl div.bien {background-color: #77EF6A;color: #000;font-size:16px;}</style></body></html> ";
+}else{ 
+    echo " <html><head></head><body> 
+		   <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
+		   <link rel='stylesheet' type='text/css' href='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.css' />
+		   <script src='//cdnjs.cloudflare.com/ajax/libs/jquery-jgrowl/1.4.1/jquery.jgrowl.min.js'></script>
+		   <script type='text/javascript'>$.jGrowl('Correo NO Enviado Correctamente',{theme : 'bien'});</script> 
+		   <style type='text/css'>div.jGrowl div.bien {background-color: #FE2E2E;color: #000;font-size:16px;}</style></body></html> ";
+}
 header("Location:index.php");
 ?> 
